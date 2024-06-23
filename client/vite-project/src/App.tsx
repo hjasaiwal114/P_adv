1400a740bd3aff0302d285494bdeae4abeaaf438
Himanshu <hjasaiwal114@gmail.com>
2024-06-23 06:09:41 +0530

    finishi forms and tables

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import { BrowswerRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Dashboard } from ",/pages/dashboard";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
 import { Auth } from "./pages/auth";
-import { FinacialRecordsProvider } from "./contexts/financial-record-context";
+import { FinancialRecordsProvider } from "./contexts/financial-record-context";
 import { SignedIn, UserButton } from "@clerk/clerk-react";
 // import { dark } from "@clerk/themes";
 
@@ -11,20 +11,22 @@ function App() {
     <Router>
       <div className="app-container">
         <div className="navbar">
-          <Link to="/">Dashboard</Link>
+          <Link to="/"> Dashboard</Link>
           <SignedIn>
             <UserButton />
           </SignedIn>
         </div>
-        <Routes 
-          path="/"
-          element={
-            <FinacialRecordsProvider>
-              <Dashboard />
-            </FinacialRecordsProvider>
-          }
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <FinancialRecordsProvider>
+                <Dashboard />
+              </FinancialRecordsProvider>
+            }
           />
           <Route path="/auth" element={<Auth />} />
+        </Routes>
       </div>
     </Router>
   );
--- client/vite-project/src/pages/dashboard/index.tsx
+++ client/vite-project/src/pages/dashboard/index.tsx
@@ -4,26 +4,25 @@ import { FinancialRecordList } from "./financial-record-list";
 import "./financial-record.css";
 import { useFinancialRecords } from "../../contexts/financial-record-context";
 import { useMemo } from "react";
+export const Dashboard = () => {
+  const { user } = useUser();
+  const { records } = useFinancialRecords();
 
-export const Dasboard = () => {
-    const { user } => useUser();
-    const { records } = useFinancialRecords();
+  const totalMonthly = useMemo(() => {
+    let totalAmount = 0;
+    records.forEach((record) => {
+      totalAmount += record.amount;
+    });
 
-    const totalMonthly = useMemo(() => {
-        let totalMonthly = 0;
-        records.forEach((record) => {
-            totalAmount += record.amount;
-        });
+    return totalAmount;
+  }, [records]);
 
-        return totalMonthly;
-    }, [records]);
-
-    return (
-        <div className="dashboard-container">
-            <h1> Welcome {user?.firstName}! where are your finance</h1>
-            <FinancialRecordForm/>
-            <div>Total Monthaly: ${totalMonthaly}</div>
-            <FinancialRecordList />
-        </div>
-    );
+  return (
+    <div className="dashboard-container">
+      <h1> Welcome {user?.firstName}! Here Are Your Finances:</h1>
+      <FinancialRecordForm />
+      <div>Total Monthly: ${totalMonthly}</div>
+      <FinancialRecordList />
+    </div>
+  );
 };
